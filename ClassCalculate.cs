81770417fad5f333d4c2a071a6b04993ec947d5f
yapovar545 <navel2485@gmail.com>
2023-09-22 13:50:02 +0300

    first

---
+++ ClassCalculate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySharp
+{
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+
+    public class Circle : IShape
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Радиус должен быть положительным числом.");
+            }
+
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        public double Side1 { get; }
+        public double Side2 { get; }
+        public double Side3 { get; }
+
+        public Triangle(double side1, double side2, double side3)
+        {
+            if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+            {
+                throw new ArgumentException("Все стороны треугольника должны быть положительными числами.");
+            }
+
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+        }
+
+        public double CalculateArea()
+        {
+            double semiPerimeter = (Side1 + Side2 + Side3) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - Side1) * (semiPerimeter - Side2) * (semiPerimeter - Side3));
+        }
+
+        public bool IsRightAngleTriangle()
+        {
+            double[] sides = { Side1, Side2, Side3 };
+            Array.Sort(sides);
+
+            return Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2);
+        }
+    }
+}
