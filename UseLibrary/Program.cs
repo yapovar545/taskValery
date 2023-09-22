795f56f43f1689fdef74acd505e2ff1ca0cfcf0b
yapovar545 <107147777+yapovar545@users.noreply.github.com>
2023-09-22 14:35:02 +0300

    Add files via upload

---
+++ UseLibrary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibrarySharp;
+
+
+namespace UseLibrary
+{
+     class Program
+    {
+        static void Main()
+        {
+            IShape shape1 = new Circle(5);
+            Console.WriteLine($"Площадь круга равна {shape1.CalculateArea()}");
+
+            IShape shape2 = new Triangle(3, 4, 5);
+            Console.WriteLine($"Площадь треугольника равна {shape2.CalculateArea()}");
+            Console.WriteLine($"Это прямоугольный треугольник: {((Triangle)shape2).IsRightAngleTriangle()}");
+        }
+    }
+}
